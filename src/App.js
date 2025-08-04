1510a1e875e757b83871f92e8bf505f10d03f604
Steve Farmer <stevefarmerjr@gmail.com>
2025-08-04 12:06:27 -0700

    Add starter React code for Lifemap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
-import "./index.css";
+import React from 'react';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Lifemap</h1>
-      <p>This is your React app deployed on AWS Amplify. Never Forget how much of a FAGGOT you are...</p>
+    <div className="app-container">
+      <h1>Welcome to Lifemap fellow Farmers</h1>
+      <p>This is your modern React app deployed via AWS Amplify.</p>
+      <p>Edit <code>src/App.js</code> and push to GitHub to see live updates.</p>
     </div>
   );
 }
