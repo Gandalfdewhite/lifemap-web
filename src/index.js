12fb519435e905860da0fbb6a35a29fc5a9be55a
Gandalfdewhite <72165732+Gandalfdewhite@users.noreply.github.com>
2025-08-04 09:47:56 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello Lifemap</h1>
+    <App />
   </React.StrictMode>
 );
