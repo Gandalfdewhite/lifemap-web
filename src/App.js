827254e4b15ca18f68a7a30f74e52c9ea2580ae4
Gandalfdewhite <72165732+Gandalfdewhite@users.noreply.github.com>
2025-08-04 13:22:36 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import LifePlannerDashboard from './components/LifePlannerDashboard'; // ← Add this import
+import LifePlannerDashboard from './components/LifePlannerDashboard';
 
 function App() {
   return (
     <div className="app-container">
       <h1>Welcome to Lifemap fellow Farmers</h1>
-      <LifePlannerDashboard /> {/https://je3r7euop3gwsiq62fe7ng3nfu0tuhtq.lambda-url.us-west-2.on.aws//}
+      <LifePlannerDashboard />
     </div>
   );
 }
