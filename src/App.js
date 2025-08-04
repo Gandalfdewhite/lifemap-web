6684adab203865cc78a23651fcf5a33df1ba8279
Gandalfdewhite <72165732+Gandalfdewhite@users.noreply.github.com>
2025-08-04 09:40:09 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Lifemap</h1>
+      <p>This is your starter React app hosted on AWS Amplify.</p>
+    </div>
+  );
+}
+
+export default App;
